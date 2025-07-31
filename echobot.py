8697618b10eb84f3c5e416c7587ff22300c53810
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-07-31 16:47:52 +0500

    Togrilandi

--- echobot.py
+++ echobot.py
@@ -5,39 +5,43 @@ from settings import TOKEN
 url_get_updates = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 url_send_message = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
-
-with open('last_update.txt', 'r') as f:
-    last_update_id = int(f.read())
+try:
+    with open('last_update.txt', 'r') as f:
+        last_update_id = int(f.read())
+except FileNotFoundError:
+    last_update_id = 0
 
 while True:
     params = {
-        'offset': last_update_id
+        'offset': last_update_id,
+        'timeout': 10
     }
-    r = requests.get(url_get_updates)
 
+    r = requests.get(url_get_updates, params=params)
     data = r.json()
-    result = data['result']
 
-    # for update in result:
-    #     msg = update['message']
-    #     user = update['message']['from']
+    result = data.get('result', [])
 
-    #     text = msg['text']
+    if not result:
+        time.sleep(1)
+        continue
 
-    #     if text == '/start':
-    #         params = {
-    #             'chat_id': user['id'],
-    #             'text': 'salom echo botga xush kelibsiz\nbu bot text yozsangiz echo qiladi.'
-    #         }
-    #         requests.get(url=url_send_message, params=params)
-    #     else:
-    #         params = {
-    #             'chat_id': user['id'],
-    #             'text': text
-    #         }
-    #         requests.get(url=url_send_message, params=params)
+    for update in result:
+        msg = update.get('message', {})
+        user = msg.get('from', {})
 
-    last_update_id = result[-1]['update_id']
-    with open('last_update.txt', 'w') as f:
-        f.write(f'{last_update_id}')
+        text = msg.get('text')
+        chat_id = user.get('id')
+
+        if text and chat_id:
+            message_params = {
+                'chat_id': chat_id,
+                'text': text
+            }
+            requests.get(url=url_send_message, params=message_params)
+
+        last_update_id = update['update_id'] + 1
+
+        with open('last_update.txt', 'w') as f:
+            f.write(str(last_update_id))
 
